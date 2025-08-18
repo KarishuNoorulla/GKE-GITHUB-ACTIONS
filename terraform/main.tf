2bd1606c74f4c217517bbe4a82f6eaa31660abf9
noorulla <noorullak007@gmail.com>
2025-08-18 13:57:23 +0530

    1 ci-cd.yml updated bucket step updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,55 @@
 provider "google" {
-  project     = var.project_id
-  region      = var.region
+  project = var.project_id
+  region  = var.region
 }
 
 provider "google-beta" {
-  project     = var.project_id
-  region      = var.region
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_compute_network" "vpc" {
+  name                    = "${var.cluster_name}-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.cluster_name}-subnet"
+  region        = var.region
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = "10.10.0.0/24"
+
+  secondary_ip_range {
+    range_name    = "pod-range"
+    ip_cidr_range = "10.20.0.0/16"
+  }
+
+  secondary_ip_range {
+    range_name    = "service-range"
+    ip_cidr_range = "10.30.0.0/16"
+  }
 }
 
 resource "google_container_cluster" "primary" {
-  provider = google-beta
-  name     = var.cluster_name
-  location = var.region
-  remove_default_node_pool = true
-  initial_node_count       = 1
-  network    = google_compute_network.vpc.name
-  subnetwork = google_compute_subnetwork.subnet.name
+  provider                 = google-beta
+  name                     = var.cluster_name
+  location                 = var.region
+  remove_default_node_pool  = true
+  initial_node_count        = 1
+  network                  = google_compute_network.vpc.name
+  subnetwork               = google_compute_subnetwork.subnet.name
+
   ip_allocation_policy {
     cluster_secondary_range_name  = "pod-range"
     services_secondary_range_name = "service-range"
   }
+
   private_cluster_config {
     enable_private_nodes    = true
     enable_private_endpoint = false
     master_ipv4_cidr_block  = "172.16.0.0/28"
   }
+
   master_authorized_networks_config {
     cidr_blocks {
       cidr_block   = "0.0.0.0/0"
@@ -38,36 +63,19 @@ resource "google_container_node_pool" "primary_nodes" {
   cluster    = google_container_cluster.primary.name
   location   = var.region
   node_count = var.node_count
+
   node_config {
     preemptible  = true
     machine_type = var.machine_type
-    disk_size_gb = 30
+    disk_type    = "pd-standard"  # Use standard disk to reduce quota usage
+    disk_size_gb = 20             # Smaller disk to fit quota
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
   }
+
   management {
     auto_repair  = true
     auto_upgrade = true
-  }
-}
-
-resource "google_compute_network" "vpc" {
-  name                    = "${var.cluster_name}-vpc"
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.cluster_name}-subnet"
-  region        = var.region
-  network       = google_compute_network.vpc.name
-  ip_cidr_range = "10.10.0.0/24"
-  secondary_ip_range {
-    range_name    = "pod-range"
-    ip_cidr_range = "10.20.0.0/16"
-  }
-  secondary_ip_range {
-    range_name    = "service-range"
-    ip_cidr_range = "10.30.0.0/16"
   }
 }
