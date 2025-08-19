1497c1129cb68414ae94bdab227291beaa7d43d7
noorulla <noorullak007@gmail.com>
2025-08-19 13:50:17 +0530

    updated files updated ymls

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
 
   node_config {
-    machine_type = "e2-medium"
+    machine_type = "e2-small"
     disk_size_gb  = 50   
     oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
